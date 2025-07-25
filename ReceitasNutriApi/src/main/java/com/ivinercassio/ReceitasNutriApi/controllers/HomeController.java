0bbb623139b9506ca65a88bf25499f7652977967
Fillipe Tomaz Carvalho Braga <125302557+FillipeTomaz@users.noreply.github.com>
2025-07-25 09:26:07 -0300

    Update HomeController.java

--- ReceitasNutriApi/src/main/java/com/ivinercassio/ReceitasNutriApi/controllers/HomeController.java
+++ ReceitasNutriApi/src/main/java/com/ivinercassio/ReceitasNutriApi/controllers/HomeController.java
@@ -1,4 +1,4 @@
-package com.seuprojeto.api.controllers;
+package com.ivinercassio.ReceitasNutriApi.controllers;
 
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;

0aa7db62859ba78c8b3597cd7cb885f60ed0d9b5
Fillipe Tomaz Carvalho Braga <125302557+FillipeTomaz@users.noreply.github.com>
2025-07-25 09:08:37 -0300

    Create HomeController.java

---
+++ ReceitasNutriApi/src/main/java/com/ivinercassio/ReceitasNutriApi/controllers/HomeController.java
@@ -0,0 +1,13 @@
+package com.seuprojeto.api.controllers;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HomeController {
+
+    @GetMapping("/")
+    public String index() {
+        return "Receita Nutri está online!";
+    }
+}
